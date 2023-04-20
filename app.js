2782c6b71bb57a9d06c479bfee5c7872d3012d43
bhavarinikhil <bhavirinikhil@gmail.com>
2023-04-20 09:02:21 +0100

    second commit

--- app.js
+++ app.js
@@ -7,44 +7,46 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-const API_KEY = "d542647e613016b046efcc0102c20196"
-
+const API_KEY ="d542647e613016b046efcc0102c20196"
+let lastCityName = "Manchester"
+//urlByLatLong = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&units=metric&appid="+API_KEY
+   
 
 app.get("/", function (request, response) {
     cityName = "Manchester"
-    urlByCity = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=metric&appid="+API_KEY
-    urlByLatLong = "https://api.openweathermap.org/data/2.5/weather?lat=&lon=&units=metric&appid="+API_KEY
-    axios.get(urlByCity)
-    .then(res => {
-        const data = res.data
-        const city = data.name;
-        const country = data.sys.country;
-        const {description, id} = data.weather[0];
-        const {temp, feels_like, humidity} = data.main;
-        let result = {}
-        if(id == 800){
-            result["wIcon"] = "../images/icons/clear.svg";
-        }else if(id >= 200 && id <= 232){
-            result["wIcon"] = "../images/icons/storm.svg";  
-        }else if(id >= 600 && id <= 622){
-            result["wIcon"] = "../images/icons/snow.svg";
-        }else if(id >= 701 && id <= 781){
-            result["wIcon"] = "../images/icons/haze.svg";
-        }else if(id >= 801 && id <= 804){
-            result["wIcon"] = "../images/icons/cloud.svg";
-        }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
-            result["wIcon"] = "../images/icons/rain.svg";
-        }
-        
-        result["temp"] = Math.floor(temp);
-        result["description"]  = description;
-        result["location"]  = `${city}, ${country}`;
-        result["feels_like"]  = Math.floor(feels_like);
-        result["humidity"]  = `${humidity}%`;
-     
-        response.render('home', { response: result });
-    })
-    .catch(err => console.log(err))
+    if(request.query.cityName!=undefined){
+        cityName=request.query.cityName
+    }
+    
+    weatherByCityName(response, cityName);
+    
+});
+
+
+app.get("/getByLatLong/:lat/:long", function (request, response) {
+    if(request.params["lat"]=='js'){
+
+    }else{
+        url="https://nominatim.openstreetmap.org/reverse?format=json&lat="+ request.params["lat"]+"&lon="+ request.params["long"]+"&zoom=18&addressdetails=1"
+        // url = "https://api.openweathermap.org/data/2.5/forecast?lat="+ request.params["lat"]+"&lon="+ request.params["long"]+"&appid="+API_KEY
+         axios.get(url)
+         .then(res => {
+             console.log(res)
+             if(res["data"]!=undefined && res["data"]["address"]!=undefined && res["data"]["address"]["city"]!=undefined){
+                lastCityName = res["data"]["address"]["city"]
+             }else if(res["data"]!=undefined && res["data"]["address"]!=undefined && res["data"]["address"]["town"]!=undefined){
+                lastCityName = res["data"]["address"]["town"]
+             }
+             response.send( lastCityName)
+             //weatherByCityName(response, lastCityName)
+                
+         })
+         .catch(err => {
+            //weatherByCityName(response, lastCityName)
+             console.log(err);
+         });
+    }
+   
     
 });
 
@@ -53,4 +55,42 @@ app.listen(3000, function (request, response) {
     console.log("Server started and listening http://localhost:3000");
 
 });
+
+function weatherByCityName(response, cityName) {
+    urlByCity = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + API_KEY;
+    urlByLatLong = "https://api.openweathermap.org/data/2.5/weather?lat=&lon=&units=metric&appid=" + API_KEY;
+    axios.get(urlByCity)
+        .then(res => {
+            const data = res.data;
+            const city = data.name;
+            const country = data.sys.country;
+            const { description, id } = data.weather[0];
+            const { temp, feels_like, humidity } = data.main;
+            let result = {};
+            if (id == 800) {
+                result["wIcon"] = "../images/icons/clear.svg";
+            } else if (id >= 200 && id <= 232) {
+                result["wIcon"] = "../images/icons/storm.svg";
+            } else if (id >= 600 && id <= 622) {
+                result["wIcon"] = "../images/icons/snow.svg";
+            } else if (id >= 701 && id <= 781) {
+                result["wIcon"] = "../images/icons/haze.svg";
+            } else if (id >= 801 && id <= 804) {
+                result["wIcon"] = "../images/icons/cloud.svg";
+            } else if ((id >= 500 && id <= 531) || (id >= 300 && id <= 321)) {
+                result["wIcon"] = "../images/icons/rain.svg";
+            }
+
+            result["temp"] = Math.floor(temp);
+            result["description"] = description;
+            result["location"] = `${city}, ${country}`;
+            result["feels_like"] = Math.floor(feels_like);
+            result["humidity"] = `${humidity}%`;
+
+            response.render('home', { response: result });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,6 @@
+const inputPart = document.querySelector(".input-part")
+const inputField = inputPart.querySelector("input")
+const locationBtn = inputPart.querySelector("button")
 
 inputField.addEventListener("keyup", e => {
 
@@ -7,9 +10,43 @@ inputField.addEventListener("keyup", e => {
 });
 
 locationBtn.addEventListener("click", () => {
+    console.log("called")
     if (navigator.geolocation) {
-
+        console.log("current pos");
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     } else {
         alert("Your browser not support geolocation api");
     }
 });
+
+
+function onSuccess(position){
+    console.log("onsuccess");
+    const {latitude, longitude} = position.coords; // getting lat and lon of the user device from coords obj
+   document.getElementById("cityName").value=latitude
+   document.getElementById("longitude").value=longitude
+
+   const Http = new XMLHttpRequest();
+    const url="http://localhost:3000/getByLatLong/"+latitude+"/"+longitude;
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        
+            console.log(Http.responseText)
+            document.getElementById("cityName").value=Http.responseText
+        
+            
+    }
+   //const form = document.createElement('form');
+ // form.method = "get";
+  //form.action = "?latitude="+latitude+"&longitude="+longitude;
+  //form.action = "getByLatLong/"+latitude+"/"+longitude;
+ // form.action = "/";
+ // document.body.appendChild(form);
+ // form.submit();
+}
+
+function onError(error){
+    console.log(error)
+}
